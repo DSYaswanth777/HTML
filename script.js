6c253dbb9594f72695fa4e13ed40cc8cb96cfcfd
DSYaswwanth777 <dsyaswanth777@gmail.com>
2026-05-26 17:25:17 +0530

    `Added JavaScript code snippets for various topics, including type conversion, basic operators, assignment operators, comparison operators, logical operators, nullish coalescing operator, conditional branching, loops, and tasks.`

---
+++ script.js
@@ -0,0 +1,157 @@
+// Type Conversion
+
+// alert("2"+"2")
+// alert(2+2)
+// alert("Rama" + " " + "Krishna")
+// implicit conversion
+// alert("2"-"1")
+
+// + is used join strings
+
+// -, *,/ work with numbers
+
+// Number("10")
+
+//Anything give in prompt is treated as string
+// let age = prompt("Enter your age");
+// console.log(age)
+// console.log(age)
+//So we are using explicit conversion
+// age = Number(age)
+// alert(age+5)
+
+// Basic Operators
+
+// let a = 6;
+// let b = 8;
+
+// console.log(a+b) //add   
+// console.log(a-b) //subtract
+// console.log(a*b)  //multiply
+// console.log(a/b) //divide
+// console.log(a % b) //remidner
+
+// // Assignment Operator
+// let count = 5;
+
+// // count = count+1;
+// ++count;
+// console.log(count)
+// count++;
+
+// //comparsion operators (lead into decisions)
+
+// console.log(5>3)
+// console.log(5<3)
+
+
+// console.log(5 === 5);
+
+// console.log(5 === "5"); 
+// console.log(5 == "5");
+
+// === compares both data type and value is called as strict equaility
+// == compares only value  this is called as equals to
+
+// Logical Operators
+
+// AND (&&)
+// OR (||)
+// NOT (!)
+
+// PUB
+
+// age > 18 && hasID && hasHardCopy
+
+// isManager || isOwner
+
+// Anyone is enough to enter
+
+// !isLoggedIn
+
+// Nullish Coalescing Operator ( ??)
+
+// let name =" ";
+// alert(name ?? "Guest")
+
+// ?? checks only null or undefined
+
+// Not 0, not " "
+
+//Conditional Branching
+
+// let age = 18;
+// let hasLicense = true;
+
+// if (age >= 18 && hasLicense) {
+//   console.log("You can Drive ");
+// } else {
+//   console.log("You can't Drive");
+// }
+
+// let marks = Number(prompt("Enter Marks"));
+
+// if (marks >= 90) {
+//   console.log("Grade A");
+// } else if (marks >= 60) {
+//   console.log("Grade B");
+// } else {
+//   console.log("Grade C");
+// }
+
+// Loops
+
+// let i = 1;
+
+// while (i <= 5) {
+//   console.log("Hello", i);
+//   i++;
+// }
+
+// for (let j = 1; j <= 5; j++) {
+//   console.log("hello for loop", j);
+// }
+
+// let i = 1;
+
+// while (i <= 5) {
+//   console.log("Hello", i);
+
+//  }
+
+// Tasks;
+
+// Print number from 1 to 10
+// print even number from 2 to 20
+
+// Ask password untill correct
+// using while loop
+
+// Ask user two number using prompt convert to number and display sum
+
+// Age validator
+
+// if age is less than 0 -> show invalid age
+// if age is 18 or more -> show "Eligible to Vote"
+// otehr wise -> show "Not eligible to vote"
+
+// Even or Odd Checker
+
+// Check whether number is Odd or even
+
+// largest of Two Numbers
+
+// Ask user two number and print the largest number
+
+// if both are equal show "Both are equal"
+
+// Default Username (Nullish Colesching)
+
+// Print Numbers from 1 to 10
+
+// using for loop
+
+// Print Even Numbers using both for and while loops
+
+// Countdown Using while
+// Print Numbers from 10 to 1
